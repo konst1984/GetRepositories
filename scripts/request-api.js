04931483a02b374fa1128cec28b8f8cd2a2774fe
Konst_V <varjutin@yandex.ru>
2022-06-23 00:30:53 +0300

    del request-api.js file

--- scripts/request-api.js
+++
@@ -1,9 +0,0 @@
-const requestAPi = async (value, perPage, page) => {
-  const response = await fetch(
-    `https://api.github.com/search/repositories?q=${value}+in:name&per_page=${perPage}&page=${page}`
-  );
-
-  return response.json();
-};
-
-export { requestAPi };
